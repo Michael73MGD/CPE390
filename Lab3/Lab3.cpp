c449ced39e537ede4f411268b4662f814714d153
Mike <michaelgdasaro@gmail.com>
2020-09-14 13:03:30 -0400

    lab3

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
-int sum(int a, int b){
-	int sum=0;
-	for(int i=a;i<=b;i++){
-		sum+=i;
-	}
-	return sum;
-}
-double prod(int a, int b){
-	double product=1;
-	for(int i=a;i<=b;i++){
-		product*=i;
-	}
-	return product;
-}
+
 
 
 int main() {
-	cout << sum(5, 8) << '\n'; // compute sum from a to b inclusive 5+6+7+8
-	cout << sum(9, 72) << '\n'; // compute sum from a to b inclusive 9+10+11+...+71+72
-	cout << prod(6, 8) << '\n'; // compute product 6*7*8
-	cout << prod(50, 68) << '\n'; //  compute 50*51*52*... * 68
+	cout << "Enter a number:" << '\n'; // compute sum from a to b inclusive 5+6+7+8
+    int n;
+    cin >> n;
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+            cout << n << " ";
+        }
+        else {
+            n=n*3+1;
+            cout <<n<< " ";
+        }
+    }
 }

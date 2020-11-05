305d9a108018f1c0146095ca8c227b246b33721f
Michael <michaelgdasaro@gmail.com>
2020-11-05 18:21:54 -0500

    more mean

--- hw13mean/mean.s
+++ hw13mean/mean.s
@@ -1,14 +1,21 @@
-.global _Z4meanii
-_Z4meanii:
-	mov r2, #0
-	vmov.f64 d0, r2
-	vmov.f64 d2, r1
+@Michael Dasaro
+
+@Note, the code works up to the divion, which I can't get to divide by the size of the array, see comments
+
+.global _Z4meanPKdi
+_Z4meanPKdi:
+	mov r2, #0		@This for incrementing through the list, and protects r1 for use in division
+	@vmov.f64 d0, #0	@It seems that d0 is automatically 0, and this doesn't compile anyway
 1:
-	vldr.f64 d1, [r0,r2] 
-	add r2, #4
-	vadd.f64 d0, d1
-	subs r1, #1
-	bgt 1b
+	vldr.f64 d1, [r0]	@load d1 with the current value in the array
+	vadd.f64 d0,d0,d1	@add the current value onto the running sum (d0)
+	add r0, #8		@increment the pointer by 8
+	add r2, #1		@increment our loop counter
+	cmp r2, r1		@check if we're at the end of the array
+	blt 1b			@loop again if current position is less than the length
 	
-	vdiv.f64 d0, d2
-	bx lr
+	vmov s2,r1		@An attempt at converting the size (r1), to a float
+	vcvt.f64.f32 d1, s2	@An attempt at converting the size (now  a float, s2) to a double, d1
+	@vdiv.f64 d0, d1	@An attempt at diving the sum by the size of the array, hopefully converted to a double
+	bx lr			@When uncommented, the above line fails to compile
+

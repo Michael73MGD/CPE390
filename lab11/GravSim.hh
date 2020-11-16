8334973419c90b9fc7e6d8ec0f65ea93dc074682
Mike <michaelgdasaro@gmail.com>
2020-11-16 13:01:31 -0500

    fasdf

--- lab11/GravSim.hh
+++ lab11/GravSim.hh
@@ -82,7 +82,7 @@ public:
 			 double vx, double vy, double vz) : m(m),
 			pos(x,y,z), v(vx, vy, vz), a() {
 		strncpy(this->name, name, 16);
-		this.Gm = G*m;
+		this->Gm = G*m;
 	}
 	friend std::ostream& operator <<(std::ostream& s, const Body& b) {
 		return s << b.name << '\t' << b.pos << '\t' << b.v << '\t' << b.a;

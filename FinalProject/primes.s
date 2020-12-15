b496807e1b1a146b1d4a5e5419276509be16e757
Michael Dasaro <michaelgdasaro@gmail.com>
2020-12-15 17:51:46 -0500

    final work

--- FinalProject/primes.s
+++ FinalProject/primes.s
@@ -1,4 +1,22 @@
 .global main
 main:
-	mov r0, #3
+	@Given p, uint32 array of size (n+1+31)/32/2 in location r0
+	@Given n, number of integers to count primes for, in r1
+	@Given size, number of bits in p, in r2
+
+	mov r3, #1		@Number of primes, to be returned, setting at 1 because assuming 2 is prime
+	@Now looping through "array" and setting all bits to true
+		mov r4, r1		@copy of address 
+		ldr r5, =0xFFFFFFFF		@32 bits of all 1s
+		mov r6, r2				@copy of size
+1:
+		str r5, [r4]			@store all 1s in this section of the array
+		add r4, #4				@increment address by 4 bytes, 32 bits
+		subs r2, #32
+		bge 1b
+	@out of loop, all bits are set to true
 	
+	
+	
+	mov r3, r0
+	bx lr

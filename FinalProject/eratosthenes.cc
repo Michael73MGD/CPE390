1ea8d57371c7157fee04502995ad8baf03f7cb2c
Michael <michaelgdasaro@gmail.com>
2020-12-15 18:29:11 -0500

    final

--- FinalProject/eratosthenes.cc
+++ FinalProject/eratosthenes.cc
@@ -45,7 +45,7 @@ uint32_t eratosthenes2(bool isPrime[], uint32_t n) {
 int main() {
   uint32_t n = 1000000000;  //1 billion
   const uint32_t size = (n+1 + 31  ) / 32 / 2;
-  cout<<"Size: "<<size<<"\n";
+  //cout<<"Size: "<<size<<"\n";
   uint32_t* p = new uint32_t[size];
   cout << eratosthenes(p, n, size) << '\n';
   delete [] p;
--- FinalProject/primes.s
+++ FinalProject/primes.s
@@ -2,18 +2,18 @@
 _Z12eratosthenesPjjj:
 	@Given p, uint32 array of size (n+1+31)/32/2 in location r0
 	@Given n, number of integers to count primes for, in r1
-	@Given size, number of bits in p, in r2
-
+	@Given size, number of ints (each 32 bits) in p, in r2
+	push {r4-r8}
 	mov r3, #1		@Number of primes, to be returned, setting at 1 because assuming 2 is prime
 	@Now looping through "array" and setting all bits to true
-		mov r4, r1		@copy of address 
+		mov r4, r1			@copy of address 
 		ldr r5, =0xFFFFFFFF		@32 bits of all 1s
-		mov r6, r2				@copy of size
+		mov r6, r2			@copy of size
 1:
 		str r5, [r4]			@store all 1s in this section of the array
-		add r4, #4				@increment address by 4 bytes, 32 bits
-		subs r2, #32
-		bge 1b
+		add r4, #4			@increment address by 4 bytes, 32 bits
+		subs r6, #1			@Lower copy of size by one int (32 bits)
+		bgt 1b				@loop until size is at or below 0
 	@out of loop, all bits are set to true
 	
 	
